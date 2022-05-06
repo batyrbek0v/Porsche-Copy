05f9354a8d5424de008f82f4d2a55d7d4ab6c566
Erlan <erlankambarov94@gmail.com>
2022-05-06 06:59:46 +0600

    adaptived the main blocks test2

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,15 @@
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    loop: true,
-    speed: 1800,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    Keyboard: true,
-  });
+	slidesPerView: 1,
+	loop: true,
+	speed: 1400,
+	pagination: {
+		el: ".swiper-pagination",
+	clickable: true,
+	},
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+	Keyboard: true,
+});
+
